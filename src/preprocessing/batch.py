Fix masking + other testing

src/preprocessing/batch.py
@@ -1,4 +1,5 @@
 import torch
+# import matplotlib.pyplot as plt
 
 # Methods
 #########
@@ -36,7 +37,20 @@ def FFT(support_query_set, config):
         support_query_set = support_query_set[:, :, 1:]
 
     if config["log_FFT"]:
-        support_query_set = torch.log(support_query_set)
+        support_query_set = torch.log1p(support_query_set)
+
+    # fig, axs = plt.subplots(2, 3, figsize=(20, 12), sharey=True)
+    # axs = axs.flatten()
+    # axs[0].plot(support_query_set[0, 0])
+    # axs[1].plot(support_query_set[1, 0])
+    # axs[2].plot(support_query_set[9, 0])
+    # axs[3].plot(support_query_set[0, 5])
+    # axs[4].plot(support_query_set[1, 5])
+    # axs[5].plot(support_query_set[9, 5])
+
+    # plt.tight_layout()
+    # plt.show()
+    # quit()
 
     return support_query_set
 

src/preprocessing/class_batch.py
@@ -14,7 +14,7 @@ def FFT(support_query_set, config):
     support_query_set = torch.abs(support_query_set)
 
     if not config["include_FFT_DC"]:
-        support_query_set = support_query_set[:, :, 1:]
+        support_query_set = support_query_set[:, 1:]
 
     if config["log_FFT"]:
         support_query_set = torch.log1p(support_query_set)

src/preprocessing/full.py
@@ -75,7 +75,8 @@ def mixed_query_normalization(train_data, validation_data, test_data, config):
             # Scale for the healthy state of each rpm
             p25 = g[sensors].quantile(0.25)
             p75 = g[sensors].quantile(0.75)
-            scale[n[1]] = (p75 - p25).astype("float32")
+            scale[n[1]] = (p75 - p25).astype("float32") * \
+                config["mixed_query_normalization_scale"]
 
         # Scaling helper
         def scale_group(group_data):
@@ -115,7 +116,8 @@ def mixed_query_normalization(train_data, validation_data, test_data, config):
                 sensor_windows = torch.tensor(
                     np.array([window[sensor].to_numpy() for window in windows]))
 
-                fft_windows = torch.abs(torch.fft.rfft(sensor_windows))
+                fft_windows = torch.abs(torch.fft.rfft(
+                    sensor_windows, norm="forward"))
                 # First log, then mean should be the correct order
                 if config["log_FFT"]:
                     fft_windows = torch.log1p(fft_windows)