3ed1b5f52dc98d852f095e86bca988e3cb80febe
Aleksanteri Hämäläinen <aleksanteri.hamalainen@aalto.fi>
2023-10-12 13:01:53 +0300

    Arotor_replication h_opt paramaters for result generation

--- slurm_scripts/arotor.sh
+++ slurm_scripts/arotor.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 #SBATCH --gres=gpu:1
-#SBATCH --time=00:55:00
+#SBATCH --time=01:55:00
 #SBATCH --mem=8G
 #SBATCH --array=1-2
 
 module restore torch
 source activate env/
 
-srun python src/result_generation.py --config arotor/result_generation_base --config_override_base arotor/result_generation/config_override --array_task_id $SLURM_ARRAY_TASK_ID --block_size 3 --repetitions 10 --job_name "arotor-{$SLURM_ARRAY_JOB_ID}"
+srun python src/result_generation.py --config arotor/result_generation_base --config_override_base arotor/result_generation/config_override --array_task_id $SLURM_ARRAY_TASK_ID --block_size 3 --repetitions 10 --job_name "arotor-$SLURM_ARRAY_JOB_ID"
--- slurm_scripts/arotor_replication_mixed.sh
+++ slurm_scripts/arotor_replication_mixed.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 #SBATCH --gres=gpu:1
-#SBATCH --time=00:55:00
+#SBATCH --time=01:55:00
 #SBATCH --mem=8G
 #SBATCH --array=1-10
 
 module restore torch
 source activate env/
 
-srun python src/result_generation.py --config arotor_replication/result_generation_mixed_base --config_override_base arotor_replication/result_generation/config_override --array_task_id $SLURM_ARRAY_TASK_ID --block_size 5 --repetitions 10 --job_name "arotor_replication_mixed-{$SLURM_ARRAY_JOB_ID}"
+srun python src/result_generation.py --config arotor_replication/result_generation_mixed_base --config_override_base arotor_replication/result_generation/config_override --array_task_id $SLURM_ARRAY_TASK_ID --block_size 5 --repetitions 10 --job_name "arotor_replication_mixed-$SLURM_ARRAY_JOB_ID"
